e55e875e96efed2d5d3f5d12dd22fedb533b8107
Benjamin Barenblat <bbarenblat@galois.com>
2014-03-20 16:15:22 -0700

    Example: Replace 'TruePath' function
    In a fit of relicensing (commit 9efa55e), I accidentally deleted
    important code from the example program.  This commit restores that
    code.

--- example/slam.py
+++ example/slam.py
@@ -97,27 +97,20 @@ init_est = numpy.ndarray(shape=(3,1), buffer=numpy.array([[float(startLong)],
                                                           [float(startAngle)]]))
 
 ##
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#   1. Redistributions of source code must retain the above copyright notice,
-#      this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#   3. Neither Galois's name nor the names of other contributors may be used to
-#      endorse or promote products derived from this software without specific
-#      prior written permission.
+# This computes an array of points that correspond to the robot's true path.
 #
-# THIS SOFTWARE IS PROVIDED BY GALOIS AND OTHER CONTRIBUTORS "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED.  IN NO EVENT SHALL GALOIS OR OTHER CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# @param dataset The Matlab data set in question.
+# @return An array of points corresponding to true positions (from DGPS).
+def TruePath(dataset):
+    result = numpy.array([[],[]])
+    longitudeRet = MakeArrayIterator(dataset['GPSLon'][0])
+    latitudeRet = MakeArrayIterator(dataset['GPSLat'][0])
+    for i in range(len(dataset['Time'][0])-1):
+        if dataset['Time'][0][i] >= dataset['Time'][0][0]+timeLimit:
+            break
+        if dataset['Sensor'][0][i] == GPS_FRAME:
+            result = numpy.append(result,[[longitudeRet()],[latitudeRet()]],1)
+    return result
 
 ##
 # This computes an array of points that correspond to blind dead reckoning.
