d8177485b1eb457dbc4427138c9b574debc4bed5
Philip Moss Robinson <probinson@localhost.localdomain>
2015-04-13 13:51:17 -0700

    register return string is now storable in bash environment variables, apparently this was alread the case for other commands/modules

--- example/tst.sh
+++ example/tst.sh
@@ -1,14 +1,12 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 rm -f index.db;
 sqlite3 index.db < ../db_init.sql;
 mv index.db ~/.local/share/ppaml/
 
-ENG=`peval register engine 1 engine`
-echo ${ENG}
-exit
-SOL=`peval register solution ${ENG} 0-0-0 solution solution/one`
-DAT=`peval register dataset 0-0-0 dataset/input/ dataset/eval/`
+ENG=$(peval register engine 1 engine 2>&1)
+SOL=$(peval register solution ${ENG} 0-0-0 solution solution/one 2>&1)
+DAT=$(peval register dataset 0-0-0 dataset/input/ dataset/eval/ 2>&1)
 
 # test registration of new configuration file
 peval register configuration ${SOL} solution/five
