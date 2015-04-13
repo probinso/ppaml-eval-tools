1b18979e57c97590885444cfe8ed29877869024d
Philip Moss Robinson <probinson@localhost.localdomain>
2015-04-13 14:48:58 -0700

    change order of file registration to make capturing of errors easier

--- example/tst.sh
+++ example/tst.sh
@@ -5,11 +5,11 @@ sqlite3 index.db < ../db_init.sql;
 mv index.db ~/.local/share/ppaml/
 
 ENG=$(peval register engine 1 engine 2>&1)
-SOL=$(peval register solution ${ENG} 0-0-0 solution solution/one 2>&1)
+SOL=$(peval register solution ${ENG} 0-0-0 solution solution/one solution/five 2>&1)
 DAT=$(peval register dataset 0-0-0 dataset/input/ dataset/eval/ 2>&1)
 
 # test registration of new configuration file
-peval register configuration ${SOL} solution/five
+peval register configuration ${SOL} solution/two_minus_one
 peval register configuration ${SOL} ten
 peval run ${ENG} ${SOL} ${DAT} #  Integrator   0
 
