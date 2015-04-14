dc94c453daabbc80b65159fad046a77d9c721f93
Philip Moss Robinson <probinson@localhost.localdomain>
2015-04-14 16:06:10 -0700

    insure that variables only store last output string

--- example/tst.sh
+++ example/tst.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
+
 rm -f index.db;
 sqlite3 index.db < ../db_init.sql;
 mv index.db ~/.local/share/ppaml/
 
-ENG=$(peval register engine 1 engine 2>&1)
-SOL=$(peval register solution ${ENG} 0-0-0 solution solution/one solution/five 2>&1)
-DAT=$(peval register dataset 0-0-0 dataset/input/ dataset/eval/ 2>&1)
+ENG=$(peval register engine 1 engine 2>&1 | tail -n 1)
+SOL=$(peval register solution ${ENG} 0-0-0 solution solution/one solution/five 2>&1 | tail -n 1)
+DAT=$(peval register dataset 0-0-0 dataset/input/ dataset/eval/ 2>&1 | tail -n 1)
 
 # test registration of new configuration file
 peval register configuration ${SOL} solution/two_minus_one
