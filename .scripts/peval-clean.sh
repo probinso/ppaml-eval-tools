b16dfe65488c05e886284f6f481e7bf5c26c6941
Philip Moss Robinson <probinson@localhost.localdomain>
2015-05-21 13:38:36 -0700

    clean stuff from peval database

---
+++ .scripts/peval-clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+sqlite3 ~/.local/share/ppaml/index.db \
+  "pragma foreign_keys = ON;
+   delete from engine;
+   delete from solution;
+   delete from dataset;"
+
+rm -rf /tmp/ppaml*
+
