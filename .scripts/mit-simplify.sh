9395f8560adcfd2c21463ba25acac4f8095cff5e
Philip Moss Robinson <probinson@localhost.localdomain>
2015-06-10 17:31:30 -0700

    script to help identify most recent submissions from mit team

---
+++ .scripts/mit-simplify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for pattern in graphs hmm homophily lda lifted pcfg qmr recursion regression cp5 venture-0.3; do
+    rm -f $( ls -t | grep ${pattern} | tail -n +2);
+done
+
+for i in $(ls -t *.tar.bz2); do 
+    label=${i%%.tar.bz2};
+    label=${label##venture-};
+    echo `md5sum $i | awk '{print $1}' ` $label `date -r "$i" +"%F"` "(4.x)"; 
+done | column -t
