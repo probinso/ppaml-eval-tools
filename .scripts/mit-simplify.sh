6ce1072f9f8b7e2926f67201e891b93b9bb80a40
Philip Robinson <pmoss.robinson@gmail.com>
2015-06-16 17:37:50 -0700

    mit would like us to evaluate both pcfg programs

--- .scripts/mit-simplify.sh
+++ .scripts/mit-simplify.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for pattern in graphs hmm homophily lda lifted pcfg qmr recursion regression cp5 venture-0.3; do
+for pattern in pcfg;
+    rm -f $( ls -t | grep ${pattern} | tail -n +3);
+done
+
+
+for pattern in graphs hmm homophily lda lifted qmr recursion regression cp5 venture-0.3;
     rm -f $( ls -t | grep ${pattern} | tail -n +2);
 done
 
