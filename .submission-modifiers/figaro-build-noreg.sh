af0827fe0c1ab5b2e100e93dcc5feff3525d3c06
probinson <pmoss.robinson@gmail.com>
2015-10-15 14:47:56 -0700

    fix directory typo

---
+++ .submission-modifiers/figaro-build-noreg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+set -x
+
+ename=figaro-3.1.0-1
+rm -rf $ename
+tar -vxjf $ename.tar.bz2
+export ENGROOT=$(readlink -m $ename/lib/)
+
+fname=figaro-CP04-2.0.0-0b2
+rm -rf $fname/
+tar -vxjf $fname.tar.bz2
+cd $fname/
+
+rm -rf lib;
+ln -s $ENGROOT lib;
+sbt clean; 
+sbt compile;
+
+for i in {1..9}; do
+  prob=prob0$i;
+  ldir=./$prob/classes/com/cra/appril;
+  rdir=./target/scala-2.11/classes/com/cra/appril;
+  mkdir -p $ldir;
+  cp -r $rdir/metrics $ldir; ## include in all problems because it's not obvious which is owner
+  cp -r $rdir/$prob   $ldir;
+
+  cd $prob;
+    CPID=04-0$i-00
+    mv data ../$prob-data
+    sed -i -- 's/..\/lib\/\*:..\/target\/scala-2.11\/classes/.\/classes/g' ./run.sh;
+
+  cd -;
+
+done;
