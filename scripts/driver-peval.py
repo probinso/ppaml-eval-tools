c4b181f1bdd62f4eb3c09127d3edb3b49901bff4
Nathan Collins <conathan@galois.com>
2016-07-21 16:13:39 -0700

    Add titles and put more important things first.
    Here "more important" is defined as "more important to Nathan" :P
    
    I don't understand what some of the "run" output means, so my titles could probably be better.

--- scripts/driver-peval.py
+++ scripts/driver-peval.py
@@ -42,30 +42,76 @@ def print_run(pps, cps, config, ds, comment=None):
     print ""
     print "peval run", pps.id, cps.id, config.id, ds.in_digest
 
-@pny.db_session
-def thething():
+def print_title(title):
+    print
+    print title
+    print "################################################################"
+    print
 
+@pny.db_session
+def print_teams():
+    print_title("Teams")
     pny.select((t.id, t.description) for t in Team).show()
 
+@pny.db_session
+def print_datasets():
+    print_title("Datasets")
+
+    print "challenge problem :: input digest :: eval digest :: input relative path"
+    print
     divider = " :: "
-    print "\n\nI AM CHECKING DATASETS \n----------------------"
     for x in pny.select(e for e in Dataset):
         for y in x.challenge_problems:
             print y, divider, x.in_digest, divider, x.eval_digest, divider, x.rel_inpath
 
+@pny.db_session
+def print_solutions():
+    print_title("Solutions")
+
+    print "team :: challenge problem :: solution digest"
+    print
+    for tch in pny.select((
+        s.engine.team.description,
+        s.challenge_problem.id,
+        s.id,
+    ) for s in Solution):
+        print "%s :: %s :: %s" % tch
+
+@pny.db_session
+def print_engines():
+    print_title("Engines")
+
+    print "team :: engine digest :: full path"
+    print
+    for x in pny.select(e for e in Engine):
+        print "%s :: %s :: %s" % (x.team.description, x.id, x.full_path)
+
+@pny.db_session
+def print_completed_run_stats():
+    print_title("Completed Run Stats")
+    for fields in pny.select((
+            r.engine.team.description,
+            r.configured_solution.solution.challenge_problem.id,
+            r.configured_solution.solution.challenge_problem.revision_major,
+            r.id,
+            r.configured_solution.solution.id[:8],
+            r.dataset.in_digest,
+            r.duration,
+            r.output[:8]) for r in Run):
+        print "%15s - CP%d.%d.0 { run.id: %03d, solution[:8]: %s, dataset: %s, took: %10.4f secs, ouput[:8]: %s }" % fields
+        #print divider.join(map(str,list(x)))
+
+
+@pny.db_session
+def print_everything():
+    print_teams()
+    print_engines()
+    print_solutions()
+    print_datasets()
 
     #print 
     #print
     #pny.select((cp, pny.count(cp.datasets)) for cp in ChallengeProblem).show()
-    """
-    print
-    print
-    pny.select((
-        s.engine.team.description,
-        s.challenge_problem.id,
-        s.id
-    ) for s in Solution).show()
-    """
 
     runs = pny.select(
       (r.id, r.configured_solution.solution, r.configured_solution, r.dataset)
@@ -84,14 +130,11 @@ def thething():
       lambda (pps, cps, con, ds): (cps, con, ds) not in [(r[1], r[2], r[3]) for r in runs]
       )
 
-    print
-    print
+    print_title("Run Command Lines")
     for pps, cps, con, ds in unran:
         print_run(pps, cps, con, ds)
 
-    print "-"*40
-    print "COMPLETED RUNS"
-    print "-"*40
+    print_title("Completed Runs")
     for pps, cps, con, ds in ran:
         print_run(
           pps, cps, con, ds,
@@ -100,6 +143,7 @@ def thething():
           ) + " times."
         )
 
+    print_completed_run_stats()
 
     """
     print "\n\nI AM CHECKING SOLUTIONS \n----------------------"
@@ -114,18 +158,6 @@ def thething():
 
 
     """
-    print "\n\nRUNS \n----------------------"
-    for fields in pny.select((
-            r.engine.team.description,
-            r.configured_solution.solution.challenge_problem.id,
-            r.configured_solution.solution.challenge_problem.revision_major,
-            r.id,
-            r.configured_solution.solution.id[:8],
-            r.dataset.in_digest,
-            r.duration,
-            r.output[:8]) for r in Run):
-        print "%15s - CP%d.%d.0 { run.id: %03d, solution[:8]: %s, dataset: %s, took: %10.4f secs, ouput[:8]: %s }" % fields
-        #print divider.join(map(str,list(x)))
 
     print "\n", "-"*40, "\n"
     runs = pny.select((r.id) for r in Run)
@@ -134,17 +166,12 @@ def thething():
     else:
       print "No runs yet."
 
-    print "\n\nEVALUATIONS \n---------------"
+    print_title("Evaluations")
     for fields in pny.select((
             e.id,
             e.run.id,
             e.meta_created) for e in Evaluation).order_by(2):
         print "%s for run %03d at %s" % fields
 
-    print "\n\nEngines \n----------------------"
-    for x in pny.select(e for e in Engine):
-        print x.team.description, "\t::\t", x.id, "\t", x.full_path
-
-
 if __name__ == "__main__":
-    thething()
+    print_everything()
